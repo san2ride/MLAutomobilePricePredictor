e29b4d9fc4be34cb5d18255495563f57f444c84e
Jason <sanchezjason@me.com>
2024-07-17 15:13:20 -0600

    carvana_price_predictor/machine_learning/playground/carvana- .json file

--- Carvana.playground/Contents.swift
+++ Carvana.playground/Contents.swift
@@ -5,16 +5,34 @@ import CreateML
 let fileURL = Bundle.main.url(forResource: "carvana", withExtension: "csv")!
 let options = CSVReadingOptions(hasHeaderRow: true, delimiter: ",")
 
-let formattingOptions = FormattingOptions(maximumLineWidth: 250, maximumCellWidth: 250, maximumRowCount: 20, includesColumnTypes: true)
+let formattingOptions = FormattingOptions(maximumLineWidth: 250, 
+                                          maximumCellWidth: 250,
+                                          maximumRowCount: 20,
+                                          includesColumnTypes: true)
+
+let calendar = Calendar.current
+let currentYear = calendar.component(.year, from: Date())
 
 let dataFrame = try DataFrame(contentsOfCSVFile: fileURL, options: options)
 
-let dataFrameUnder60KMiles = dataFrame.filter(on: "Miles", Int.self) { miles in
+let dataSliceUnder60KMiles = dataFrame.filter(on: "Miles", Int.self) { miles in
     guard let miles = miles else { return false }
     return miles <= 60000
 }
 
-print(dataFrameUnder60KMiles.description(options: formattingOptions))
+let dataSliceLessThan5YearsOld = dataSliceUnder60KMiles.filter(on: "Year", Int.self) { year in
+    guard let year = year else { return false }
+    return currentYear - year >= 0 && currentYear - year <= 5
+}
+
+let dataSliceToyotaAndJeep = dataSliceLessThan5YearsOld.filter(on: "Name", String.self) { name in
+    guard let name = name else { return false}
+    return name.contains("Toyota") || name.contains("Jeep")
+}
+
+//print(dataSliceToyotaAndJeep.description(options: formattingOptions))
+
+let carvanaDataFrame = DataFrame(dataSliceToyotaAndJeep)
 
 let regressor = try MLLinearRegressor(trainingData: dataFrame, targetColumn: "Price")
 
@@ -22,4 +40,19 @@ let metaData = MLModelMetadata(author: "Jason Sanchez",
                                shortDescription: "Carvana Model",
                                license: nil, version: "1.0",
                                additional: nil)
-// try regressor.write(toFile: "/Users/jasonspro/Develop/MLAutomobilePricePredictor/Carvana.mlmodel", metadata: metaData)
+//try regressor.write(toFile: "/Users/jasonspro/Develop/MLAutomobilePricePredictor/Carvana.mlmodel", metadata: metaData)
+
+let nameDataFrame = carvanaDataFrame.selecting(columnNames: ["Name"])
+let nameColumnSlice = nameDataFrame["Name"].distinct()
+let uniqueNames: [String] = nameColumnSlice.compactMap {
+    ($0 as? String)?.trimmingCharacters(in: .whitespacesAndNewlines)
+}
+
+let nameColumn = Column(name: "name", contents: uniqueNames)
+
+var uniqueNameDataFrame = DataFrame()
+uniqueNameDataFrame.append(column: nameColumn)
+
+print(uniqueNameDataFrame)
+
+try uniqueNameDataFrame.writeJSON(to: URL(fileURLWithPath: "/Users/jasonspro/Develop/MLAutomobilePricePredictor/CarNames.json"))
